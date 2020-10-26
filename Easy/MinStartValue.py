d327ac2bc6759381f6d9c47a9eb187b81730bb1c
AvivSham <Mista2311@gmail.com>
2020-10-26 16:52:41 +0200

    min start value sol

---
+++ Easy/MinStartValue.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def min_start_value(nums: List[int]) -> int:
+    t = 0
+    m = float('inf')
+
+    for i in nums:
+        m = min(m, t)
+        t += i
+
+    return abs(min(m, t)) + 1
+
+
+if __name__ == '__main__':
+    n = [3, 2, 3, 4, 2]
+    print(min_start_value(n))
