8b5c335e2270daa52ea638bd7db62d55614baf56
AvivSham <Mista2311@gmail.com>
2020-11-28 10:19:12 +0200

    fix

--- Easy/MaxProfit.py
+++ Easy/MaxProfit.py
@@ -19,30 +19,6 @@ def maxProfit(prices: List[int]) -> int:
                 buy_p = 0
     return prof
 
-def sol(prices: List[int]) -> int:
-
-    maxp = 0
-
-    # corner case
-    if not prices or not prices[1:]: return maxp
-
-    buy = None
-
-    for i in range(1, len(prices)):
-
-        if prices[i] >= prices[i - 1]:  # determine the buy time
-            if buy is None:
-                buy = prices[i - 1]
-            if i == len(prices) - 1:  # sell when reach the end
-                maxp = maxp + prices[i] - buy
-
-        if i <= len(prices) - 2 and prices[i] > prices[i + 1]:  # determine the sell time
-            if buy is not None:
-                maxp = maxp + prices[i] - buy
-                buy = None
-
-    return maxp
-
 
 if __name__ == '__main__':
     print(maxProfit([7, 1, 5, 3, 6, 4]))
